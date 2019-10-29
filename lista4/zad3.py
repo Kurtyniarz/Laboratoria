21c32e83c2cd5b4567e237e599142ce5246b5d22
Igor B <ibockowsky@gmail.com>
2019-10-29 15:49:56 +0100

    Poprawa zadania 3

--- lista4/zad3.py
+++ lista4/zad3.py
@@ -1,6 +1,16 @@
 def norecursive(word):
     return word == word[::-1]
 
+def norec2(word):
+    czyjest = True
+    dlg = len(word)
+    for i in range(0, int(dlg/2)):
+        if czyjest:
+            if word[i] != word[dlg - 1 - i]:
+                czyjest = False
+        else:
+            break
+    return czyjest
 
 def recursive(word):
     if len(word) < 2:
@@ -16,6 +26,11 @@ if norecursive(a):
     print('(bez rekurencji)Jest palindromem.')
 else:
     print('(bez rekurencji)Nie jest palindromem.')
+
+if norec2(a):
+    print('(bez rekurencji2)Jest palindromem.')
+else:
+    print('(bez rekurencji2)Nie jest palindromem.')
 
 if recursive(a):
     print('(rekurencja)Jest palindromem.')
