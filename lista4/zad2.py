0b8f4819e757445c9e3d05dc61fab22ee731ea96
= <=>
2019-11-05 13:54:32 +0100

    Nowa lista

--- lista4/zad2.py
+++ lista4/zad2.py
@@ -1,17 +1,23 @@
 def trojkat(bok):
+    if bok <= 1:
+        return print('Za mala liczba.')
     for i in range(0, bok):
         row = ''
-        for n in range(0, i+1):
+        for n in range(0, i + 1):
             if n == 0:
                 row += '*'
             elif n != i:
                 row += ' '
             elif n == i:
-                if i == bok-1:
-                    row = (i+1) * '*'
+                if i == bok - 1:
+                    row = (i + 1) * '*'
                 else:
                     row += '*'
         print(row)
 
-a = int(input('Podaj bok trojkata: '))
-trojkat(a)
+
+try:
+    a = int(input('Podaj bok trojkata: '))
+    trojkat(a)
+except ValueError:
+    print('Zly format.')
