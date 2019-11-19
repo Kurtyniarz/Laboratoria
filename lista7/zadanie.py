39c2b298c2a824c96a19bd94acc2d7b4a74250e6
= <=>
2019-11-19 14:00:03 +0100

    Lista 7

---
+++ lista7/zadanie.py
@@ -0,0 +1,61 @@
+import pickle
+import time
+
+
+class Blog:
+    def __init__(self, author):
+        if author == '':
+            author = 'Default User'
+        self.author = author
+        file = open('blogdata.txt', 'rb')
+        self.blogData = pickle.load(file)
+        print(self.blogData)
+        file.close()
+
+    def run(self):
+        self.load()
+        print('Wybierz operacje:')
+        print('1.Dodaj wpis')
+        print('2.Zakoncz program')
+        opt = input('... ')
+        self.option(opt)
+
+    def option(self, option):
+        if option == 1 or option == '1':
+            self.add()
+        elif option == 2 or option == '2':
+            self.exit()
+        else:
+            print('Zly wybor...')
+            self.run()
+
+    def add(self):
+        print('Podaj wpis, ktory chcesz dodac: ')
+        text = str(input())
+        newDate = time.ctime(time.time())
+        self.blogData['entries'].append({
+            'author': self.author,
+            'text': text,
+            'date': str(newDate)
+        })
+        file = open('blogdata.txt', 'wb')
+        pickle.dump(self.blogData, file)
+        self.run()
+
+    def load(self):
+        for entry in reversed(self.blogData['entries']):
+            print(f"Autor: {entry['author']}, Data i czas: {entry['date']} \n Wpis: {entry['text']}")
+
+    def exit(self):
+        exit()
+
+
+def main():
+    print('Witaj w aplikacji Blog! Podaj swoje imie')
+    author = str(input('Imie: '))
+    newBlog = Blog(author)
+    newBlog.run()
+
+
+if __name__ == '__main__':
+    main()
